b986c1801c9574a0ac894d20cf03beb576ecdf5d
Akash Biswas <130483894+HOWCANIHELPI@users.noreply.github.com>
2023-08-24 22:21:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+/*let from=document.querySelector('form');
+let input=document.querySelectorAll('.input');
+let name=document.querySelector('.namee');
+
+
+
+
+ from.onsubmit = () => {
+   alert(`Thank You ${name.value}`)
+
+   input.forEach((curElem) => {
+     curElem.value = '';
+   });
+ }
+*/
